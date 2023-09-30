95bee6878fecc3b103b38980c390e65017e33630
Asterisk <casnz1601@gmail.com>
2023-09-30 18:09:28 -0400

    update

--- main.py
+++ main.py
@@ -3,8 +3,6 @@ import sqlite3
 
 # Load the iris dataset into the SQLite database
 def load_iris(dataset="iris.csv"):
-    print("-" * 50)
-    print("Loading data from", dataset)
     conn = sqlite3.connect('iris.db')
     cursor = conn.cursor()
     cursor.execute('DROP TABLE IF EXISTS iris')
@@ -14,71 +12,53 @@ def load_iris(dataset="iris.csv"):
         SepalLengthCm DOUBLE
     )
     ''')
-    with open(dataset, 'r') as csv_file:
+    with open(dataset, 'r', encoding="utf-8") as csv_file:
         csv_reader = csv.DictReader(csv_file)
         for row in csv_reader:
             cursor.execute('INSERT INTO iris (SepalLengthCm) VALUES (?)', (row["SepalLengthCm"],))
     conn.commit()
     conn.close()
-    print("Data loaded successfully!")
-    print("-" * 50)
 
 # CRUD functions for the iris dataset
 def create_iris_entry(sepal_length):
-    print("Creating new entry with SepalLengthCm =", sepal_length)
     with sqlite3.connect('iris.db') as conn:
         cursor = conn.cursor()
         cursor.execute("INSERT INTO iris (SepalLengthCm) VALUES (?)", (sepal_length,))
         conn.commit()
-    print("Entry created successfully!")
-    print("-" * 50)
 
 def read_all_iris_entries():
-    print("Reading all entries...")
     with sqlite3.connect('iris.db') as conn:
         cursor = conn.cursor()
         cursor.execute("SELECT * FROM iris")
-        entries = cursor.fetchall()
-    print("Entries:", entries)
-    print("-" * 50)
-    return entries
+        return cursor.fetchall()
 
 def read_iris_entry_by_id(entry_id):
-    print("Reading entry with ID =", entry_id)
     with sqlite3.connect('iris.db') as conn:
         cursor = conn.cursor()
         cursor.execute("SELECT * FROM iris WHERE id=?", (entry_id,))
-        entry = cursor.fetchone()
-    print("Entry:", entry)
-    print("-" * 50)
-    return entry
+        return cursor.fetchone()
 
-def update_iris_entry(id, sepal_length):
-    print(f"Updating entry with ID = {id} to SepalLengthCm = {sepal_length}")
+def update_iris_entry(entry_id, sepal_length):
     with sqlite3.connect('iris.db') as conn:
         cursor = conn.cursor()
-        cursor.execute("UPDATE iris SET SepalLengthCm=? WHERE id=?", (sepal_length, id))
+        cursor.execute("UPDATE iris SET SepalLengthCm=? WHERE id=?", (sepal_length, entry_id))
         conn.commit()
-    print("Entry updated successfully!")
-    print("-" * 50)
 
-def delete_iris_entry(id):
-    print(f"Deleting entry with ID = {id}")
+
+def delete_iris_entry(entry_id):
     with sqlite3.connect('iris.db') as conn:
         cursor = conn.cursor()
-        cursor.execute("DELETE FROM iris WHERE id=?", (id,))
+        cursor.execute("DELETE FROM iris WHERE id=?", (entry_id,))
         conn.commit()
-    print("Entry deleted successfully!")
-    print("-" * 50)
 
 def main():
     load_iris()
     create_iris_entry(5.5)
-    read_all_iris_entries()
+    print(read_all_iris_entries())
     update_iris_entry(1, 6.5)
-    read_iris_entry_by_id(1)
+    print(read_iris_entry_by_id(1))
     delete_iris_entry(1)
-    read_all_iris_entries()
+    print(read_all_iris_entries())
 
 if __name__ == "__main__":
     main()
